41b7b3201fa5a3a82fe927d2d01757260e863e84
Vincent140206 <vincentiusdylan01@gmail.com>
2025-10-27 14:32:47 +0700

    add projectStatusController

--- routes/projectAnalysisRoutes.js
+++ routes/projectAnalysisRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const projectAnalysisController = require('../controller/projectAnalysisController');
+const projectStatusController = require('../controller/projectStatusController'); 
 
 router.post('/analyze/:projectID', projectAnalysisController.analyzeProject);
 router.post('/form/analyze/:id', projectAnalysisController.analyzeProject);
