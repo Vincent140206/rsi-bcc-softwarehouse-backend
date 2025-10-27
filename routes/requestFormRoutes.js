db88c60a555dc837a510614d3ebc87f45c3e3a8f
Vincent140206 <vincentiusdylan01@gmail.com>
2025-10-27 15:12:26 +0700

    change endpoint for get project by user id

--- routes/requestFormRoutes.js
+++ routes/requestFormRoutes.js
@@ -25,6 +25,6 @@ router.post('/form/submit', async (req, res) => {
 
 router.get('/form/data', requestFormController.getAllRequests);
 router.get('/form/:requestId', requestFormController.getProjectDetail);
-router.get('/form/:userId', requestFormController.getAllByUserId);
+router.get('/form/user/:userId', requestFormController.getAllByUserId);
 
 module.exports = router;
