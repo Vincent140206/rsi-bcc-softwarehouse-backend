ddb91351f7a4062e452aa99f389ca7de867de609
Vincent140206 <vincentiusdylan01@gmail.com>
2025-10-31 17:06:18 +0700

    change ProjectMembers models

--- models/ProjectMembers.js
+++ models/ProjectMembers.js
@@ -1,26 +1,38 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
+const Project = require('./Project');
+const Member = require('./Member');
 
 const ProjectMembers = sequelize.define('ProjectMembers', {
   projectId: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    primaryKey: true,
     references: {
-      model: 'Projects',
+      model: Project,
       key: 'id'
-    }
+    },
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
   },
   memberId: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    primaryKey: true,
     references: {
-      model: 'Members',
+      model: Member,
       key: 'id'
-    }
+    },
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
   }
 }, {
   tableName: 'ProjectMembers',
-  timestamps: false
+  timestamps: false,
+  freezeTableName: true,
+  createdAt: false,
+  updatedAt: false,
+  id: false
 });
 
 module.exports = ProjectMembers;
