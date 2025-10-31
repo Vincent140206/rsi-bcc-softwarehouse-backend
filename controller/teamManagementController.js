5cdf171aa06f5a4f42c184350a8f25af5b65982e
Vincent140206 <vincentiusdylan01@gmail.com>
2025-10-31 14:13:04 +0700

    add error debugging

--- controller/teamManagementController.js
+++ controller/teamManagementController.js
@@ -1,15 +1,14 @@
 const Member = require('../models/Member');
 
 exports.getMemberList = async (req, res) => {
-    try {
-        const members = await Member.findAll({ where: { status: 'available' } });
-
-        if (members.length === 0) {
-            return res.status(404).json({ success: false, message: 'No available members found' });
-        }
-        
-        res.status(200).json({ success: true, data: members });
-    } catch (error) {
-        res.status(500).json({ success: false, message: 'Internal Server Error' });
+  try {
+    const members = await Member.findAll({ where: { status: 'available' } });
+    if (members.length === 0) {
+      return res.status(404).json({ success: false, message: 'No available members found' });
     }
-};  
+    res.status(200).json({ success: true, data: members });
+  } catch (error) {
+    console.error('Error in getMemberList:', error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
