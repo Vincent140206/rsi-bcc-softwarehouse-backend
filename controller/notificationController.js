649e69d148aa496f8d701f1d68f91ed3e8e19429
Vincent140206 <vincentiusdylan01@gmail.com>
2025-10-31 13:56:54 +0700

    rename function

--- controller/notificationController.js
+++ controller/notificationController.js
@@ -1,7 +1,7 @@
 const Notification = require('../models/Notification');
 const Member = require('../models/Member');
 
-exports.getNotificationsForMember = async (req, res) => {
+exports.getNotifications = async (req, res) => {
     try {
         const memberId = req.params.memberId;
         const notifications = await Notification.findAll({ where: { memberId } }, { include: Member });
