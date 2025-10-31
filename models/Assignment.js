574cbbbdd84dbab9b21add1ea1b1521dd4c6e74d
Vincent140206 <vincentiusdylan01@gmail.com>
2025-10-31 15:03:38 +0700

    change logic for sequelize

--- models/Assignment.js
+++ models/Assignment.js
@@ -1,16 +1,39 @@
-module.exports = (sequelize, DataTypes) => {
-  const Assignment = sequelize.define('Assignment', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    projectId: DataTypes.INTEGER,
-    memberId: DataTypes.INTEGER,
-    role: DataTypes.STRING,
-    status: { type: DataTypes.STRING, defaultValue: 'active' }
-  });
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+const Member = require('./Member');
 
-  Assignment.associate = (models) => {
-    Assignment.belongsTo(models.Project, { foreignKey: 'projectId' });
-    Assignment.belongsTo(models.Member, { foreignKey: 'memberId' });
-  };
+const Notification = sequelize.define('Notification', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
+  memberId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Members',
+      key: 'id'
+    }
+  },
+  message: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  isRead: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  }
+}, {
+  tableName: 'Notifications',
+  timestamps: false
+});
 
-  return Assignment;
-};
+Notification.belongsTo(Member, {
+  foreignKey: 'memberId',
+  targetKey: 'id',
+  onDelete: 'CASCADE'
+});
+
+module.exports = Notification;
