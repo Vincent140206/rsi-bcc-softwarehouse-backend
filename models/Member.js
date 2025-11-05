b789c01274bf72611c86613177c8b959d2b7f5e5
Vincent140206 <vincentiusdylan01@gmail.com>
2025-11-05 08:51:21 +0700

    declare Project

--- models/Member.js
+++ models/Member.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
+const Project = require('./Project');
 
 const Member = sequelize.define('Member', {
   id: {
