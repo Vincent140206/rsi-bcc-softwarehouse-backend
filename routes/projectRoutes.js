b9626d9049197a0db37c09221f91c12ccab612dc
Vincent140206 <vincentiusdylan01@gmail.com>
2025-11-06 11:45:34 +0700

    ubah

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const projectController = require('../controllers/projectController');
+const projectController = require('../controller/projectController');
 
 router.put('/:id', projectController.updateProgress);
 router.get('/:id', projectController.getProjectWithProgress);
