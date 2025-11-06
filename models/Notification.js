3a17a8c50e1dbd1ee02f289f91f26927a9602a42
Vincent140206 <vincentiusdylan01@gmail.com>
2025-11-06 11:56:39 +0700

    change notification model

--- models/Notification.js
+++ models/Notification.js
@@ -1,24 +1,45 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 const Member = require('./Member');
+const Project = require('./Project');
 
 const Notification = sequelize.define('Notification', {
   id: {
     type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+    autoIncrement: true,
+    primaryKey: true
   },
   senderId: {
     type: DataTypes.INTEGER,
-    allowNull: false
+    allowNull: false,
+    references: {
+      model: Member,
+      key: 'id'
+    },
+    onDelete: 'NO ACTION',
+    onUpdate: 'CASCADE',
+    name: 'fk_notification_sender'
   },
   receiverId: {
     type: DataTypes.INTEGER,
-    allowNull: false
+    allowNull: false,
+    references: {
+      model: Member,
+      key: 'id'
+    },
+    onDelete: 'NO ACTION',
+    onUpdate: 'CASCADE',
+    name: 'fk_notification_receiver'
   },
   projectId: {
     type: DataTypes.INTEGER,
-    allowNull: true
+    allowNull: true,
+    references: {
+      model: Project,
+      key: 'id'
+    },
+    onDelete: 'SET NULL',
+    onUpdate: 'CASCADE'
   },
   message: {
     type: DataTypes.TEXT,
@@ -31,11 +52,21 @@ const Notification = sequelize.define('Notification', {
   parentId: {
     type: DataTypes.INTEGER,
     allowNull: true
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false
   }
 }, {
-  timestamps: true,
-  tableName: 'notifications'
+  tableName: 'notifications',
+  timestamps: true
 });
+
+module.exports = Notification;
 
 Notification.belongsTo(Member, { as: 'sender', foreignKey: 'senderId' });
 Notification.belongsTo(Member, { as: 'receiver', foreignKey: 'receiverId' });
